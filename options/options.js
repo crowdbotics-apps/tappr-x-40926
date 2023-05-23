3f95508bd8cd7bedcd4b139a05647f47a26f37e8
David Din <daviddin01@yahoo.com>
2023-05-23 10:47:43 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "tappr-x-40926",
   "url": "https://tappr-x-40926.botics.co",
   "api": "https://tappr-x-40926.botics.co/api/v1",
-  "initialRoute": "Untitled4"
+  "initialRoute": "Untitled5"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
